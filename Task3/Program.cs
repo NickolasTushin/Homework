2c115457ccd901cf1e605f8c59f59c101044beaa
Nickolas <kovcheg.studio.2014@gmail.com>
2022-11-29 18:01:48 +0300

    UPD add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0) 
+{
+   Console.WriteLine($"Ваше число {a} четное");
+}
+else 
+{
+    Console.WriteLine($"Ваше число {a} не четное");
+}
